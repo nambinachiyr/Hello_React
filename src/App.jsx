3cac0d5f192cdbbc9f1f895e51b63687bc21d031
Nambi Nachiyar <nambi2731@gmail.com>
2025-07-12 19:34:09 +0530

    moving the component to another file

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,15 @@
-// Basic 
-function Greeting(){
-  return(
-    <>
-       <h1>Heloo</h1>
-       <p>Your name?</p>
-    </>
-  )
-}
+// /Bacic
+
+import Greeting from "./Components/Greetin";
+
 
 function App(){
-  // This is one way
-  // ----------------
-  /*return <div>
-    <h1>Heloo</h1>
-    <p>This is the para</p>
-  </div>*/
-// ____________________________
-  // This is another way 
-  // --------------------
-  /*return (
-    <div>
-      <h1>Helo</h1>
-      <p>This is a para</p>
-    </div>
-  )*/
-  // ______________________________
-  // This is another way this is called fragment tag
-  /*return (
-    <>
-       <h1>Helo</h1>
-       <p>This is para</p>
-    </>
-  )*/
-
-
-    // Creating one more component
-    return(
-      // Call that component like functions in js
-      <> {/* This tag is important*/}
-        <Greeting />
-         {/* We can call multiple time as we want */}
-        <Greeting />
-        <Greeting />
-     </>
-    )
+ return (
+  <>
+    <Greeting />
+    <Greeting />
+  </>
+ )
 }
 
 export default App;
---
+++ src/Components/Greetin.jsx
@@ -0,0 +1,10 @@
+function Greeting(){
+   return (
+    <>
+      <h1>Hello</h1>
+      <p>What is your age?</p>
+    </>
+   )
+}
+
+export default Greeting
