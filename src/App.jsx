4ca36e4aff40a90090cedbbe7cf3b993a3c6ec0d
Nambi Nachiyar <nambi2731@gmail.com>
2025-07-12 19:24:33 +0530

    create multiple component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,49 @@
 // Basic 
+function Greeting(){
+  return(
+    <>
+       <h1>Heloo</h1>
+       <p>Your name?</p>
+    </>
+  )
+}
+
 function App(){
-     return <h1>Hello React</h1>
+  // This is one way
+  // ----------------
+  /*return <div>
+    <h1>Heloo</h1>
+    <p>This is the para</p>
+  </div>*/
+// ____________________________
+  // This is another way 
+  // --------------------
+  /*return (
+    <div>
+      <h1>Helo</h1>
+      <p>This is a para</p>
+    </div>
+  )*/
+  // ______________________________
+  // This is another way this is called fragment tag
+  /*return (
+    <>
+       <h1>Helo</h1>
+       <p>This is para</p>
+    </>
+  )*/
+
+
+    // Creating one more component
+    return(
+      // Call that component like functions in js
+      <> {/* This tag is important*/}
+        <Greeting />
+         {/* We can call multiple time as we want */}
+        <Greeting />
+        <Greeting />
+     </>
+    )
 }
 
 export default App;
