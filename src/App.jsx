a70b3559bc67c9f6dd8a3545225b6dd9ec4b6fdc
Nambi Nachiyar <nambi2731@gmail.com>
2025-07-12 20:40:54 +0530

    props-passing data parent to child

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 // /Bacic
-
+// Parent Cpmponent to Greeting
 import Greeting from "./Components/Greetin";
 
+// Passing a property parent to child using Props- method
 
 function App(){
+  // // If have only one 
+  //  const name = "krish"
+
+  // if have multiple
+  // const details = {
+  //   name:"nambi",
+  //   email:"nambi12@gmail.com",
+  //   signedUp:"yes"
+  // }
+
+  let name = "Nambi"
+  let email = "nambi12@gmail.com"
  return (
   <>
-    <Greeting />
-    <Greeting />
+    {/* <Greeting nam = {name}/> */}
+    {/* <Greeting details = {details}/> */}
+    <Greeting name = {name} email = {email}/>
   </>
  )
 }
--- src/Components/Greetin.jsx
+++ src/Components/Greetin.jsx
@@ -1,16 +1,31 @@
-function Greeting(){
-
+// Child component of App
+// function Greeting(props){
+// destructruce
+function Greeting({name,email}){
+//    console.log(nam) -> {nam: 'krish'} - 
+    //  console.log(props)
+     /* 
+     {details: {…}}
+     details: {name: 'nambi', email: 'nambi12@gmail.com', signedUp: true}
+     [[Prototype]]: Object
+     */
+    console.log(name)
     let num1 = 15;
     let num2 = 17;
     let age = ()=>num1+num2;
    return (
     <>
-      <h1>Hello</h1>
-      <p>What is your age?</p>
+    {/* Like to access js Object */}
+      {/* <h1>Hello,{props.details.name} !</h1>
+      <p>My email is : {props.details.email}</p>
+      <p>Logged In : {props.details.signedUp}.</p> */}
 
-      <p>My age is {age()}</p>
+      {/* if we want destrcture */}
+      <h1>Hello,{name} !</h1>
+      <p>My email is : {email}</p>
+      {/* <p>Logged In : {props.details.signedUp}.</p>  */}
     </>
    )
 }
 
-export default Greeting
+export default Greeting;
