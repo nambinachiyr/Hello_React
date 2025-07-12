060fe2db7c9eec3935ac389548a2aaef55bdeae2
Nambi Nachiyar <nambi2731@gmail.com>
2025-07-12 19:39:48 +0530

    adding js in the UI

--- src/Components/Greetin.jsx
+++ src/Components/Greetin.jsx
@@ -1,8 +1,14 @@
 function Greeting(){
+
+    let num1 = 15;
+    let num2 = 17;
+    let age = num1+num2;
    return (
     <>
       <h1>Hello</h1>
       <p>What is your age?</p>
+      
+      <p>My age is {age}</p>
     </>
    )
 }
