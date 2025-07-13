9d0d882392d35d5ef04ddcd631d0339a4f2c321c
Nambi Nachiyar <nambi2731@gmail.com>
2025-07-13 19:17:21 +0530

    fetching data to UI using state method and Effect method

--- src/App.jsx
+++ src/App.jsx
@@ -3,38 +3,28 @@
 import { useEffect, useState } from "react"
 
 function App () {
-   let [count,setCount] = useState(0)
-   function re_render(){
-       setCount(count+1);
-       console.log(count)
-   }
-   
-   // This effect run after the first render
-   //1. this is first way with depentancy []
-//   useEffect(()=>{
-//       fetch(`https://686be60114219674dcc688b8.mockapi.io/todoApp`)
-//       .then(res=>res.json())
-//       .then(data=>console.log(data))
-//   },[])
-// ------------------------------------------------------------------------
-//2. This second way without dep-[];
-// this is will also like without useEffect
-//   useEffect(()=>{
-//       fetch(`https://686be60114219674dcc688b8.mockapi.io/todoApp`)
-//       .then(res=>res.json())
-//       .then(data=>console.log(data))
-//   })
-// -------------------------------------------------------------------------
-// 3.set the depentacy array when it run inside the []
+   // This is for set the data to UI
+   // Create an empty array for putting all datas
+   // set the data in state
+   let [todos,setTodos] = useState([])
+  
+ 
   useEffect(()=>{
       fetch(`https://686be60114219674dcc688b8.mockapi.io/todoApp`)
       .then(res=>res.json())
-      .then(data=>console.log(data))
-  },[count])
+      .then(data=>setTodos(data))
+  },[])
+  console.log(todos)
    return(
      <>
-       <h1>Fetch Data in console</h1>
-       <button onClick={re_render}>re-render</button>
+       <h1>Fetch Data in console </h1>
+       <ul>
+         {
+            // key is manutary for react to find that particular element
+            // for managing purpose
+            todos.map(todos => <li key={todos.id}>{todos.description}</li>)
+         }
+       </ul>
      </>
    )
 }
