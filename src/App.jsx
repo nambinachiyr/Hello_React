3153505db7889abd2dc56617e73385d111907f9f
Nambi Nachiyar <nambi2731@gmail.com>
2025-07-13 15:00:00 +0530

    multiple useState

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,27 @@ import { useState } from "react"
 
 
 function App () {
-   // let count = 0 -> this is ordinary variable so this is not to allows to change the UI element
-    
-   // Use useState function
-   let [count,setCount] = useState(0)
+     
+   // Use multipleStates useState function
+  
+  let [like,setLike] = useState(0)
+  let [dislike,setDislike] = useState(0)
 
-   function handleClick(){
-      // count ++:Not work in UI
-      
-      console.log("clicked",count)
-      setCount(count+1)
+   function likeClick(){
+     
+      console.log("clicked",like)
+      setLike(like+1)
    }
-   function decreaseFun(){
-      console.log(count,"decrease")
-      setCount(count-1)
+   function dislikeFun(){
+      setDislike(dislike+1)
    }
-   function resetFun(){
-      console.log(count,"reset")
-      setCount(count=0)
-   }
+   
   return (
     <>
-      <h1>Count : {count}</h1>
-      <button onClick={handleClick}>Increase</button>
-      <button onClick={decreaseFun}>Decrease</button>
-      <button onClick={resetFun}>reset</button>   
+      <h1>likes & dislike</h1>
+      <button onClick={likeClick}>Like {like}</button>
+      <button onClick={dislikeFun}>Dislike {dislike}</button>
+     
 
     </>
 
