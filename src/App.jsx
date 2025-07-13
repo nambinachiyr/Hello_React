626d8b3e343c4cd4a8859314455c0d507d855630
Nambi Nachiyar <nambi2731@gmail.com>
2025-07-13 17:08:48 +0530

    Using useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
 // /Bacic
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 function App () {
    let [count,setCount] = useState(0)
    function re_render(){
-       setCount(count+1)
+       setCount(count+1);
+       console.log(count)
    }
-   fetch(`https://686be60114219674dcc688b8.mockapi.io/todoApp`)
-   .then(res=>res.json())
-   .then(data=>console.log(data))
+   
+   // This effect run after the first render
+   //1. this is first way with depentancy []
+//   useEffect(()=>{
+//       fetch(`https://686be60114219674dcc688b8.mockapi.io/todoApp`)
+//       .then(res=>res.json())
+//       .then(data=>console.log(data))
+//   },[])
+// ------------------------------------------------------------------------
+//2. This second way without dep-[];
+// this is will also like without useEffect
+//   useEffect(()=>{
+//       fetch(`https://686be60114219674dcc688b8.mockapi.io/todoApp`)
+//       .then(res=>res.json())
+//       .then(data=>console.log(data))
+//   })
+// -------------------------------------------------------------------------
+// 3.set the depentacy array when it run inside the []
+  useEffect(()=>{
+      fetch(`https://686be60114219674dcc688b8.mockapi.io/todoApp`)
+      .then(res=>res.json())
+      .then(data=>console.log(data))
+  },[count])
    return(
      <>
        <h1>Fetch Data in console</h1>
