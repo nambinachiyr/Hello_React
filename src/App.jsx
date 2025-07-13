20b8e2796974746adbbe5759cae7072c6f3c48cf
Nambi Nachiyar <nambi2731@gmail.com>
2025-07-13 16:05:52 +0530

    useState Using Array

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,20 @@
 
 import { useState } from "react"
 
-// Single state
-
-//one  useState function with mulitiple variable create
 function App () {
 
-    let [reaction,setReaction] = useState({
-      like:0,
-      dislike:0
-    })
+    let [reaction,setReaction] = useState([0,0])
     console.log(reaction)
     function likeFun(){
-      // If we have two variable it is ok but whatif if we have 10 vari
-      // setReaction({like:reaction.like+1,dislike:reaction.dislike})
-
-      // this way
-      setReaction({...reaction,like:reaction.like+1})
-      // console.log(reaction.like,"- Likes",reaction.dislike,"- Dislike")
+      setReaction([reaction[0]+1,reaction[1]])
     }
     function dislikeFun(){
-      // setReaction({dislike:reaction.dislike+1, like:reaction.like})
-
-       setReaction({...reaction,dislike:reaction.dislike+1})
-      // console.log(reaction.like,"- Likes",reaction.dislike,"- Dislike")
+     setReaction([reaction[0],reaction[1]+1])
     }
 
    return(
       <>
-        <button onClick={likeFun}>Like {reaction.like}</button> <button onClick={dislikeFun}>DisLike {reaction.dislike}</button>
+        <button onClick={likeFun}>Like {reaction[0]}</button> <button onClick={dislikeFun}>DisLike {reaction[1]}</button>
       </>
    )
 }
