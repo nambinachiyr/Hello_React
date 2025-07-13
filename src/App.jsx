12e410640e59d70d2e01be6dd75ee79ff3d20ea5
Nambi Nachiyar <nambi2731@gmail.com>
2025-07-13 22:28:19 +0530

    basic of set jsx element

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,16 @@
 import { useEffect, useState } from "react"
 
 function App () {
-   // This is for set the data to UI
-   // Create an empty array for putting all datas
-   // set the data in state
-   let [todos,setTodos] = useState([])
-  
- 
-  useEffect(()=>{
-      fetch(`https://686be60114219674dcc688b8.mockapi.io/todoApp`)
-      .then(res=>res.json())
-      .then(data=>setTodos(data))
-  },[])
-  console.log(todos)
-   return(
-     <>
-       <h1>Fetch Data in console </h1>
-       <ul>
-         {
-            // key is manutary for react to find that particular element
-            // for managing purpose
-            todos.map(todos => <li key={todos.id}>{todos.description}</li>)
-         }
-       </ul>
-     </>
-   )
+   let ele = [];
+   ele.push(<h1>hello</h1>,<p>hi</p>)
+   console.log(ele)
+  return(
+   <div>
+   {
+    ele 
+   }
+   </div>
+  )
 }
 
 export default App
