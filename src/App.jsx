d35c104bdd723a0a32aa1275dacaab789e6ee562
Nambi Nachiyar <nambi2731@gmail.com>
2025-07-14 08:04:46 +0530

    Controled component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,32 @@
 
-// This is Uncontroled component
-// Because we are not using the state to manage the input values
+// This is controled component
+
+import { useEffect, useState } from "react"
+
+// Because we are using the state to manage the input values
 function App () {
+ 
+   const [email,setEmail] = useState('')
+   const [password,setPassword] = useState('')
 
    function handleLogin(e){
-      e.preventDefault()
-      console.log(e.target.email.value,e.target.password.value)
-
+      e.preventDefault()  
+      console.log(email,password)
+        
    }
+
+// just for checking purpose
+   //  useEffect(()=>{
+   //        console.log(email)
+   //    },[email]) 
   return(
    <>
      <h3>Form Login</h3>
      <form onSubmit={()=>handleLogin(event)}>
-        <input type="email" placeholder="email.." name="email"/>
+        <input type="email" placeholder="email.." name="email" value={email} onChange={e=>(setEmail(e.target.value))}/>
         <br/>
         <br/>
-        <input type="password" placeholder="password.." name="password"/>
+        <input type="password" placeholder="password.." name="password"value={password} onChange={e=>setPassword(e.target.value)} />
         <br/>
         <br/>
         <button>Login</button>
