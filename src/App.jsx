28a0d8e8e0a456e04a254ac76a0fefbdc6d564ab
Nambi Nachiyar <nambi2731@gmail.com>
2025-07-17 08:34:36 +0530

    useReducer with one state value--beginning

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,39 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useReducer ,useEffect} from 'react'
 
 function App() {
-    
-   let [refresh,setRefresh] = useState(1)
-   let refreshB = ()=>{
-       setRefresh(refresh+1)
-       console.log(refresh,"refresh count")
-   }
 
-    let count = 0
-   const normal = ()=>{
-       count = count+1;
-       console.log(count,"normal-count")
-   }
+    // Using useReducer
+    // useReducer is also the state value manageing but this is manage complex values
+    let [like,dispatch] = useReducer(reducer,0)
+     
+    // This function runs when dispatch is called
+    // This function has two parameter
+    // One is the initial state value,one is action for change the initial value
+    // 
+    function reducer(state,action){
+        // console.log(action); //{type: 'LIKE'}
+        // console.log(state)  //0
 
-   let count1 = useRef(0)
-   const useref = ()=>{
-      count1.current = count1.current+1
-      console.log(count1.current,"ref-count")
-   }
-  
-
-   let [count2,setCount2] = useState(0)
-   const use = ()=>{
-       setCount2(count2+1);
-       console.log(count2,"state-count")
-   }
+        if(action.type == "LIKE"){
+            // The return statement will be assign to the state value
+            // like the setLike()
+            return state+1;
+        }
+    }
+    // This is use for console the like increasing
+    useEffect(()=>{
+        console.log(like)
+    },[like])
+    function hande(){
+        // Call that function inside the click function
+        // Put the argument for action parameter
+       dispatch({type:"LIKE"});
+    }
   return (
-    <div >
-       <h1>Refresh the Componetent</h1>
-       <button onClick={refreshB}>Refresh</button>  <button onClick={normal}>N-Increase</button>  <button onClick={useref}>UR-Increase</button>  <button onClick={use}>US-Increase</button>
-       <br/>     
-       <br/>     
-       <span>(Ordinary Variable - {count})</span> | <span>(UseRef variable - {count1.current})</span> | <span>(Usestate variable - {count2})</span>
-       </div>
+   <>
+      <h3>Like: {like}</h3>
+      <button onClick={hande}>Like</button>
+   </>
   )
 }
 
