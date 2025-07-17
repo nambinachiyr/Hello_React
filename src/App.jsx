8201159424417cb90bb60faee11cb08775071648
Nambi Nachiyar <nambi2731@gmail.com>
2025-07-17 11:30:49 +0530

    Redecer with multiple variable with one state

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import { initialValue, reducer } from './Reducer/R_out_of_comp'
 
 function App() {
     //  Separete function
-    let [like,dispatch] = useReducer(reducer,initialValue)  
+    let [reaction,dispatch] = useReducer(reducer,initialValue)  
   return (
    <>
-      <h3>Like: {like}</h3>
-      <button onClick={()=>dispatch({type:"LIKE"})}>Like</button>
-      <button onClick={()=>dispatch({type:"DISLIKE"})} style={{margin:10}}>disLike {like}</button>
+      <h3>Like: </h3>
+      <button onClick={()=>dispatch({type:"LIKE"})}>Like {reaction.like}</button>
+      <button onClick={()=>dispatch({type:"DISLIKE"})} style={{margin:10}}>disLike {reaction.dislike}</button>
 
    </>
   )
--- src/Reducer/R_out_of_comp.js
+++ src/Reducer/R_out_of_comp.js
@@ -1,12 +1,22 @@
 
-let initialValue = 0;
+let initialValue = {
+    like:0,
+    dislike:0
+};
 
 function reducer(state,action){
     if(action.type == "DISLIKE"){
-        return state-1;
+        return {
+            ...state,
+            dislike: state.dislike+1
+        };
     }
     else if(action.type == "LIKE"){
-        return state+1;
+        return {
+            ...state,
+            like:state.like+1
+        }
+
     }
     return state;// This is current state
 }
