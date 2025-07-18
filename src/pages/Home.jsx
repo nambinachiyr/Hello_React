8e9b8e99ff51e2a5c437c086a4d85e5e609c78ad
Nambi Nachiyar <nambi2731@gmail.com>
2025-07-18 07:58:45 +0530

    Using --- Link Component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 const Home = () => {
   return (
     <div>
-        <h1>Hello From Home</h1>
+        <h3>Go to</h3>
+        {/* This is flickering the full page it is not smooth */}
+       {/* <li><a href='/Register'>Register</a></li>
+       <li><a href='/Login'>Login</a></li> */}
+
+       {/* Use Link Component for smooth application */}
+       <li><Link to ='/Register'>Register</Link></li>
+       <li><Link to ='/Login'>Login</Link></li>
     </div>
   )
 }
